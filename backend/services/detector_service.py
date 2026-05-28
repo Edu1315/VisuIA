7e63c32054fa752ed27cdfb3f84e7b6b2384973a
Tais Capilé Ramires <taiscapile10@gmail.com>
2026-05-28 13:44:33 -0300

    reorganizacao da arquitetura do projeto-criacao de pastas para divisão de responsabilidades

---
+++ backend/services/detector_service.py
@@ -0,0 +1,14 @@
+import time
+import random
+
+
+def realizar_calculo_ia(arquivo):
+    """
+    Simula a análise de uma imagem.
+
+    Futuramente, essa função será substituída pela chamada da API real
+    de detecção de imagens geradas por IA.
+    """
+    time.sleep(1)
+
+    return random.choice(["IA", "Autêntica"])
